be5f7535f63ca7c0c8db42e2b13318dc2d9e7fb4
roza <rozanawzad2001@gmail.com>
2022-12-02 15:43:21 +0300

    done js phase 1 arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+var num1=2
+var num2= 31
+var multiply=num1 * num2
 
-// Write your code here
+var random = Math.floor(Math.random()+1)
+
+var num3=49
+var num4=5
+var mod= num3%num4
+
+var max= Math.max(2,12,20)
